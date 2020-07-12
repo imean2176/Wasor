8ac82f58472afe178043a3ea18b8d2ce4fb7fdfc
Đức Tiến <ductien.uit@gmail.com>
2020-07-12 18:17:19 +0700

    add intro screen

---
+++ app/src/main/java/com/wasor/IntroActivity.java
@@ -0,0 +1,79 @@
+package com.wasor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import com.codemybrainsout.onboarder.AhoyOnboarderActivity;
+import com.codemybrainsout.onboarder.AhoyOnboarderCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntroActivity extends AhoyOnboarderActivity {
+    private static final String TAG = IntroActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_intro);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+
+        AhoyOnboarderCard firstScreen = new AhoyOnboarderCard(getString(R.string.first_title), getString(R.string.first_description), R.drawable.ic_first);
+        firstScreen.setBackgroundColor(R.color.black_transparent);
+        firstScreen.setTitleColor(R.color.white);
+        firstScreen.setDescriptionColor(R.color.grey_200);
+        firstScreen.setTitleTextSize(dpToPixels(10, this));
+        firstScreen.setDescriptionTextSize(dpToPixels(8, this));
+        firstScreen.setIconLayoutParams(256, 256, 40, 0, 0, 40);
+
+        AhoyOnboarderCard secondScreen = new AhoyOnboarderCard(getString(R.string.second_title), getString(R.string.second_description), R.drawable.ic_second);
+        secondScreen.setBackgroundColor(R.color.black_transparent);
+        secondScreen.setTitleColor(R.color.white);
+        secondScreen.setDescriptionColor(R.color.grey_200);
+        secondScreen.setTitleTextSize(dpToPixels(10, this));
+        secondScreen.setDescriptionTextSize(dpToPixels(8, this));
+        secondScreen.setIconLayoutParams(256, 256, 40, 0, 0, 40);
+
+        AhoyOnboarderCard thirdScreen = new AhoyOnboarderCard(getString(R.string.third_title), getString(R.string.third_description), R.drawable.icon_four);
+        thirdScreen.setBackgroundColor(R.color.black_transparent);
+        thirdScreen.setTitleColor(R.color.white);
+        thirdScreen.setDescriptionColor(R.color.grey_200);
+        thirdScreen.setTitleTextSize(dpToPixels(10, this));
+        thirdScreen.setDescriptionTextSize(dpToPixels(8, this));
+        thirdScreen.setIconLayoutParams(256, 256, 40, 0, 0, 40);
+
+
+
+
+
+
+        setGradientBackground();
+
+        List<AhoyOnboarderCard> pages = new ArrayList<>();
+        pages.add(firstScreen);
+        pages.add(secondScreen);
+        pages.add(thirdScreen);
+
+        setOnboardPages(pages);
+
+        //setColorBackground(R.color.colorPrimary);
+
+        showNavigationControls(true);
+
+        setInactiveIndicatorColor(R.color.colorAccent);
+        setActiveIndicatorColor(R.color.white);
+
+        setFinishButtonTitle("Thử ngay");
+
+        setFinishButtonDrawableStyle(ContextCompat.getDrawable(this, R.drawable.rounded_button));
+    }
+    @Override
+    public void onFinishButtonPressed() {
+        Intent showMainIntent = new Intent(IntroActivity.this, MainActivity.class);
+        startActivity(showMainIntent);
+    }
+}
--- app/src/main/java/com/wasor/MainActivity.java
+++ app/src/main/java/com/wasor/MainActivity.java
@@ -2,13 +2,46 @@ package com.wasor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+//Màn hình đầu tiên mà ứng dụng hiện lên
+//Ứng dụng sẽ kiểm tra xem người dùng đã xem Màn hình giới thiệu chưa,
+//Nếu có thì chuyển qua Màn hình giới thiệu (IntroActivity).
+//Nếu không thì tiếp tục hiển thị màn hình Chính này (MainActivity).
 public class MainActivity extends AppCompatActivity {
 
+    //Biến để lưu dữ liệu người dùng.
+    SharedPreferences sharedPreferences;
+
+    // onCreate sau đó mới đến onStart
+    // Tạo màn hình trước (onCreate), rồi sau đó hiển thị màn hình đó (onStart)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedPreferences = this.getSharedPreferences("PREF", Context.MODE_PRIVATE);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        //Biến này kiểm tra xem đây có phải lần đầu khách hàng truy cập vào màn hình chính hay không
+        boolean isFirstLogin = sharedPreferences.getBoolean("isFirstLogin", false);
+
+        //Nếu lần đầu, thì cho khách hàng xem màn hình giới thiệu đầu tiên
+        if(!isFirstLogin)
+        {
+            //Cập nhật lại biến "Lần đầu truy cập" thành lần 2 truy cập, để lần sau khách hàng vào lại sẽ không hiển thị màn hình intro nữa.
+            sharedPreferences.edit().putBoolean("isFirstLogin", true).apply();
+
+            //Hiển thị màn hình giới thiệu
+            startActivity(new Intent(this, IntroActivity.class));
+        }
+
+        // Nếu không phải lần đầu thì vẫn hiển thị màn hình chính.
     }
 }
